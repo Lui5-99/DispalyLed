aedfcdbddc40e6560533ac3466ae5c7d56fa3b03
jadvp <adrian.vp.13@gmail.com>
2019-10-15 19:53:23 -0500

    Se agregaron los metodos de la clase Archivo

--- DisplayLed/DisplayLed/Archivo.cs
+++ DisplayLed/DisplayLed/Archivo.cs
@@ -3,12 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
 
-namespace DisplayLed
+namespace displayLed
 {
     class Archivo
     {
-        public int columnas { get; set; }
-        public int renglones { get; set; }
+        public void ValidarExtension(string ruta)
+        {
+            string extension = System.IO.Path.GetExtension(ruta); //Devuelve la extensión de la ruta especificada
+            try
+            {
+                if (extension.Equals(".txt"))
+                    EsVacio(ruta);
+                else
+                    Console.WriteLine("La extensión del archivo es incorrecta");
+            }
+            catch
+            {
+                Console.WriteLine("Verifique que la extensión sea correcta");
+            }
+
+        }
+
+        public void EsVacio(string ruta)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(ruta))
+                {
+                    if (sr.Peek() > 0)
+                        LeerArchivo(ruta);
+                    else
+                        Console.WriteLine("El archivo está vacío");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Verifique que el archivo seleccionado sea el correcto");
+            }
+
+        }
+
+        public void LeerArchivo(string ruta)
+        {
+            List<string> ledsLista = new List<string>();
+            //string[] ledsMatriz = new string[0];
+            Regex simbolosValidos = new Regex("^[0-X]{1,300}$");
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(ruta))
+                {
+                    string linea;
+                    while ((linea = sr.ReadLine()) != null)
+                    {
+                        if (simbolosValidos.IsMatch(linea))
+                            ledsLista.Add(linea);
+                        else
+                            Console.WriteLine("Hay simbolos no válidos en el archivo");
+                    }
+                    string[] ledsArreglo = ledsLista.ToArray();
+                    Console.WriteLine(ledsArreglo[0]);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("El archivo no pudo ser leído");
+                Console.WriteLine(e.Message);
+            }
+        }
+
     }
 }
